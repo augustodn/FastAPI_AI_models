035d8e4dfd1aa17bb35285c882364fb43404e358
Augusto de Nevrezé <augusto.denevreze@mercadolibre.com>
2021-12-30 17:58:30 -0300

    added dataset splitting before training

--- classification/multiclass/iris_training.py
+++ classification/multiclass/iris_training.py
@@ -3,8 +3,9 @@ import pandas as pd
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.utils import np_utils
-from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+
 
 # load dataset
 dataframe = pd.read_csv("iris.data", header=None)
@@ -20,17 +21,26 @@ dummy_y = np_utils.to_categorical(encoded_Y)
 
 # define baseline model
 def baseline_model():
-	# create model
-	model = Sequential()
-	model.add(Dense(8, input_dim=4, activation='relu'))
-	model.add(Dense(3, activation='softmax'))
-	# Compile model
-	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-	return model
+    # create model
+    model = Sequential()
+    model.add(Dense(8, input_dim=4, activation='relu'))
+    model.add(Dense(3, activation='softmax'))
+    # Compile model
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
 
 model = baseline_model()
-model.fit(X, dummy_y, epochs=200, batch_size=5, verbose=0, shuffle=True)
-scores = model.evaluate(X, dummy_y, verbose=0)
+
+# Split dataset in train and test
+X_train, X_test, y_train, y_test = train_test_split(
+    X, dummy_y, test_size=0.2, random_state=42)
+
+model.fit(
+    X_train, y_train, epochs=200, batch_size=5,
+    verbose=0, shuffle=True
+)
+
+scores = model.evaluate(X_test, y_test, verbose=0)
 print(f"{model.metrics_names[1]}: {scores[1]*100}")
 
 model.save('iris_model.h5')
